affbd5999310fdfe431d9935175319b45a7110db
M-WRI <me@mwri.de>
2022-09-12 22:49:32 +0200

    navigation screen done

--- components/Layout.component.tsx
+++ components/Layout.component.tsx
@@ -1,8 +1,15 @@
+// COMPONENTS
+import { Navigation } from "./Navigation.component";
 // TYPES
 import { ILayoutProps } from "../types/layout.types";
 // STYLES
 import styles from "../styles/Layout.module.scss";
 
 export const Layout = ({ children }: ILayoutProps) => {
-  return <main className={styles.container}>{children}</main>;
+  return (
+    <>
+      <Navigation />
+      <main className={styles.container}>{children}</main>
+    </>
+  );
 };
